7dd35ad1465540e87957b6967595616c44ea95d1
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-25 13:49:03 +0200

    1.1-fail

--- calculator.spec.js
+++ calculator.spec.js
@@ -4,4 +4,7 @@ describe("1. addStrings", () => {
   it("1.0 should return 0 for an empty string", () => {
     expect(addStrings("")).toBe(0);
   });
+  it("1.1 should return 1 for an '1'", () => {
+    expect(addStrings("1")).toBe(1);
+  });
 });
