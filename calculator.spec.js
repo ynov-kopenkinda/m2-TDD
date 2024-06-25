508d90bffd3dda29ec5ef1b0454efa4c06dbbeaa
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-25 14:12:56 +0200

    5.1-fail

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,6 +1,6 @@
 import { addStrings } from "./calculator";
 
-describe.skip("1. addStrings", () => {
+describe("1. addStrings", () => {
   it("1.0 should return 0 for an empty string", () => {
     expect(addStrings("")).toBe(0);
   });
@@ -12,7 +12,7 @@ describe.skip("1. addStrings", () => {
   });
 });
 
-describe.skip("2. addStrings", () => {
+describe("2. addStrings", () => {
   it("2.0 should handle any number of arguments", () => {
     expect(addStrings("1,2,3")).toBe(6);
     expect(addStrings("1,3")).toBe(4);
@@ -23,7 +23,7 @@ describe.skip("2. addStrings", () => {
   });
 });
 
-describe.skip("3. addStrings", () => {
+describe("3. addStrings", () => {
   it("3.0 Allow the add method to handle newlines as separators, instead of comas", () => {
     expect(addStrings("1\n2,3")).toBe(6);
     expect(addStrings("1\n2\n3")).toBe(6);
@@ -31,7 +31,7 @@ describe.skip("3. addStrings", () => {
   });
 });
 
-describe.skip("4. addStrings", () => {
+describe("4. addStrings", () => {
   it("Add validation to not to allow a separator at the end", () => {
     expect(() => addStrings(",")).toThrow();
     expect(() => addStrings("1,")).toThrow();
@@ -48,5 +48,9 @@ describe("5. addStrings", () => {
     expect(addStrings("//;\n1;2;3")).toBe(6);
     expect(addStrings("//;\n1;2;3;4")).toBe(10);
     expect(addStrings("//;\n1;2;3;4;5")).toBe(15);
+  });
+  it("5.1 Allow the add method to handle a different delimiter <format: //[delimiter]\n[numbers]>", () => {
+    expect(addStrings("//|\n1|2")).toBe(3);
+    expect(addStrings("//test\n1test2,3")).toBe(6);
   });
 });
