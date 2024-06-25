a98e652422d5d2b3884da0226c723de50b6605e2
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-25 14:11:42 +0200

    5.0-pass

--- calculator.js
+++ calculator.js
@@ -1,9 +1,25 @@
 export function addStrings(values) {
   if (values === "") return 0;
-  if (values.endsWith(",") || values.endsWith("\n")) {
+  let cleanValue = values.trimStart();
+  let extraDelimiter;
+  const hasExtraDelimeter = /\/\/(.*)\n/.test(cleanValue);
+  if (hasExtraDelimeter) {
+    cleanValue = cleanValue.slice(2);
+    const nextIndex = cleanValue.indexOf("\n");
+    extraDelimiter = cleanValue.slice(0, nextIndex);
+    values = cleanValue.slice(nextIndex + 1);
+  }
+  if (
+    values.endsWith(",") ||
+    values.endsWith("\n") ||
+    (extraDelimiter && values.endsWith(extraDelimiter))
+  ) {
     throw new Error("Invalid input");
   }
-  const unformValues = values.replace(/\n/g, ",");
+  let unformValues = values.replace(/\n/g, ",");
+  if (extraDelimiter) {
+    unformValues = unformValues.replace(new RegExp(extraDelimiter, "g"), ",");
+  }
   const numberStrings = unformValues.split(",");
   const numbers = numberStrings.map((x) => +x);
   const summ = numbers.reduce((a, b) => a + b, 0);
