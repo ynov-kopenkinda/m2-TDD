cf20dddf3596babee50196188df35ae220a591aa
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-25 13:57:18 +0200

    4.0-fail

--- calculator.spec.js
+++ calculator.spec.js
@@ -30,3 +30,14 @@ describe("3. addStrings", () => {
     expect(addStrings("1,2,3")).toBe(6);
   });
 });
+
+describe("4. addStrings", () => {
+  it("Add validation to not to allow a separator at the end", () => {
+    expect(addStrings(",")).toThrow();
+    expect(addStrings("1,")).toThrow();
+    expect(addStrings("\n")).toThrow();
+    expect(addStrings("1\n")).toThrow();
+    expect(addStrings("1,2,3,")).toThrow();
+    expect(addStrings("1,2,3\n")).toThrow();
+  });
+});
