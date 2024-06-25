a041824b01ed5da482bc438e3840c7d5b97662d0
Dmitrii Kopenkin (s-dkopenkin@digikare.com) <s-dkopenkin@digikare.com>
2024-06-25 13:53:38 +0200

    2.0-pass

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 export function addStrings(values) {
-  if (values === "1,2") return 3;
-  if (values === "1") return 1;
-  return 0;
+  if (values === "") return 0;
+  const numberStrings = values.split(",");
+  const numbers = numberStrings.map((x) => +x);
+  const summ = numbers.reduce((a, b) => a + b, 0);
+  return summ;
 }
